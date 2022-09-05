9da84ff2b632c35e4e72cbadcef40ec184fa2c72
Suraj Veer Talreja <63415845+SurajVeerTalreja@users.noreply.github.com>
2022-09-05 14:43:51 +0200

    Updated Secret Key

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ load_dotenv()  # take environment variables from .env.
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
+app.config['SECRET_KEY'] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
